b022ad82670d1eee5ac1a91acc747b42667a7c5c
Naren <32117832+rupy9999@users.noreply.github.com>
2019-08-12 23:57:17 +0530

    Update gateway.sh

--- gateway.sh
+++ gateway.sh
@@ -10,6 +10,6 @@ sudo sh -c "echo 'vm.swappiness = 0' >> /etc/sysctl.conf"
 sudo chmod +x /etc/rc.d/rc.local
 sudo sh -c "echo 'echo never > /sys/kernel/mm/transparent_hugepage/defrag' >> /etc/rc.local"
 sudo sh -c "echo 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' >> /etc/rc.local"
-wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
 sudo yum -y localinstall jdk-8u131-linux-x64.rpm
 java -version
