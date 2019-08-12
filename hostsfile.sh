f78b7e103a7a112d351d13c7db3f5dff01feaf26
Naren <32117832+rupy9999@users.noreply.github.com>
2019-08-12 23:29:25 +0530

    Update hostsfile.sh

--- hostsfile.sh
+++ hostsfile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+sudo sh -c "echo '10.0.0.109 manager.rupeshorg.net manager' >> /etc/hosts"
 sudo sh -c "echo '10.0.0.86 master.rupeshorg.net master' >> /etc/hosts"
 sudo sh -c "echo '10.0.0.186 snn.rupeshorg.net snn' >> /etc/hosts"
 sudo sh -c "echo '10.0.0.126 slave1.rupeshorg.net slave1' >> /etc/hosts"
 sudo sh -c "echo '10.0.0.175 slave2.rupeshorg.net slave2' >> /etc/hosts"
 sudo sh -c "echo '10.0.0.241 slave3.rupeshorg.net slave3' >> /etc/hosts"
 sudo sh -c "echo '10.0.0.115 gateway.rupeshorg.net gateway' >> /etc/hosts"
-sudo sh -c "echo '10.0.0.109 manager.rupeshorg.net manager' >> /etc/hosts"
